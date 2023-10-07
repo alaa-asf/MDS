c07488bb4519bfd78110ebdea37ef8ad96a6161d
MohammadAjam1997 <ajamm9644@gmail.com>
2023-10-07 22:04:58 +0300

    add api

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,17 +3,21 @@ import { CommonModule } from '@angular/common';
 import { PrimeNgModule } from './primeng.module';
 import { DatomSpinnerComponent } from './components/datom-spinner/datom-spinner.component';
 import {HighchartsChartModule} from "highcharts-angular";
+import { FilterDate } from './pipe-service/filter-date.pipe';
 
 
 
 @NgModule({
-  declarations: [DatomSpinnerComponent],
+  declarations: [DatomSpinnerComponent,FilterDate],
   imports: [
     CommonModule,
     PrimeNgModule,
       HighchartsChartModule,
 
   ],
-  exports:[PrimeNgModule,DatomSpinnerComponent,HighchartsChartModule]
+  providers: [
+    FilterDate
+],
+  exports:[PrimeNgModule,DatomSpinnerComponent,HighchartsChartModule,FilterDate]
 })
 export class SharedModule { }
