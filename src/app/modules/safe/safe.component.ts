5183c36f0bee1750e537bac6977f2526e85cefec
Alaa Asfahani <alaaasfahani@gmail.com>
2023-10-18 21:05:09 +0300

    update safe

--- src/app/modules/safe/safe.component.ts
+++ src/app/modules/safe/safe.component.ts
@@ -10,26 +10,72 @@ export class SafeComponent implements OnInit {
     visible: boolean = false;
     safeBalanceIQD: string = '';
     safeBalanceUSD: string = '';
-    products = [
-
+    products: [] = [];
+    transactionTypes = [
+        { name: 'ايداع', value: 'CR_SAFE' },
+        { name: 'سحب', value: 'DB_SAFE' },
     ];
+    transactionType: any = { name: 'ايداع', value: 'CR_SAFE' };
+    transactionNames: any;
+    transactionName: any;
+    transactionSides: any;
+    transactionSide: any;
+    newSafe: any = {
+        transactionType: "DB_SAFE",
+        transactionName: "ADD_SAFE",
+        transactionEntity: "12040",
+        transactionAmountIQD: 123123,
+        transactionAmountUSD: 1000,
+        notes: "the ttttttttttttttttttttttttttt",
+        accountantBoxId: 2012
+    }
     constructor(private safe: Safe) { }
 
     ngOnInit() {
         this.safe.getAllSafe().subscribe((res: any) => {
             console.log(res);
-
-            this.products = res
-           
-
+            this.products = res;
         });
         this.safe.getSafeBalance().subscribe((res: any) => {
-            this.safeBalanceIQD = res.safeBalanceIQD
-            this.safeBalanceUSD = res.safeBalanceUSD
+            this.safeBalanceIQD = res.safeBalanceIQD;
+            this.safeBalanceUSD = res.safeBalanceUSD;
         })
+        this.gettransactionSides();
+        this.getTransactionName(this.transactionType);
     }
     showDialog() {
         this.visible = true;
     }
-  
+
+    getTransactionName(transactionType: any) {
+        this.safe.getTransactionName(transactionType.value).subscribe(data => {
+            this.transactionNames = data;
+            console.log(this.transactionName);
+
+        })
+    }
+
+    gettransactionSides() {
+        this.safe.getAllacctbox().subscribe(data => {
+            this.transactionSides = data;
+            console.log(data);
+            
+        })
+    }
+
+    addSafe() {
+        this.newSafe.transactionType = this.transactionType.value;
+        this.newSafe.transactionName = this.transactionName;
+        this.newSafe.transactionEntity = this.transactionName;
+        this.newSafe.transactionAmountIQD = this.transactionName;
+        this.newSafe.transactionAmountUSD = this.transactionName;
+        this.newSafe.notes = this.transactionName;
+        this.newSafe.accountantBoxId = this.transactionName;
+        this.safe.addSafe(this.newSafe).subscribe(res => {
+            console.log(res);
+            
+        })
+    }
+
+
 }
